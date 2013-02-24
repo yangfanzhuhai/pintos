1635efcb79c47c01406ea93cdff2aa7232263a94
Luke Shilleto <luke.shilleto11@imperial.ac.uk>
2013-02-24 17:03:01 +0000

    Implemented basic system handler and method to check if user pointers are safe.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,7 +3,6 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
-#include "threads/vaddr.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -16,16 +15,74 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  void *esp = f->esp;
-  if (esp == NULL || !is_user_vaddr (esp) || 
-        pagedir_get_page (active_pd (), esp) == NULL) 
-        {
-          // Kill process and release resources
-          thread_exit ();
-        }
-  
-        
-        
-  printf ("system call!\n");
-  thread_exit ();
+  esp = f->esp;
+
+  if (!(check_ptr_valid (esp) && check_ptr_valid (esp + 1) 
+     && check_ptr_valid (esp + 2) && check_ptr_valid (esp + 3)))
+  {
+    //Maybe change for sys_exit when implemented
+    thread_exit ();
+  }
+  else
+  {
+    syscall_number = *esp;
+    switch (syscall_number)
+    {
+      case SYS_HALT:
+        halt ();
+        break;
+      case SYS_EXIT:
+        exit (*(esp + 1));
+        break;
+      case SYS_EXEC:
+        f->eax = exec ((char *) *(esp + 1));
+        break;
+      case SYS_WAIT:
+        f->eax = wait (*(esp + 1));
+        break;
+      case SYS_CREATE:
+        f->eax = create ((char *) *(esp + 1), *(esp + 2));
+        break;
+      case SYS_REMOVE:
+        f->eax = remove ((char *) *(esp + 1));
+        break;
+      case SYS_OPEN:
+        f->eax = open ((char *) *(esp + 1));
+        break;
+      case SYS_FILESIZE:
+	      f->eax = filesize (*(esp + 1));
+	      break;
+      case SYS_READ:
+        f->eax = read (*(esp + 1), (void *) *(esp + 2), *(esp + 3));
+        break;
+      case SYS_WRITE:
+        f->eax = write (*(esp + 1), (void *) *(esp + 2), *(esp + 3));
+        break;
+      case SYS_SEEK:
+        seek (*(esp + 1), *(esp + 2));
+        break;
+      case SYS_TELL:
+        f->eax = tell (*(esp + 1));
+        break;
+      case SYS_CLOSE:
+        close (*(esp + 1));
+        break;
+      default:
+        break;
+    }
+  }
+
 }
+
+bool check_ptr_valid (const void *ptr)
+{
+
+  //Maybe change for static uint32_t *active_pd (void) in pagedir.c
+  uint32_t *pd = thread_current()->pagedir;
+
+  return ptr != NULL && is_user_vaddr (ptr) && 
+         pagedir_get_page (pd,ptr)) != NULL;
+}
+
+
+
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -2,5 +2,6 @@
 #define USERPROG_SYSCALL_H
 
 void syscall_init (void);
+bool check_ptr_valid (const void *ptr);
 
 #endif /* userprog/syscall.h */
