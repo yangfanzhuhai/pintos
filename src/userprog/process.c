c3ffcd3d6fa9fd06a9c6bf1644b8ba1d26692bb1
Yangfan <yangfanzhuhai@gmail.com>
2013-02-26 07:30:04 +0800

    draft code for stack setup

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -22,6 +22,7 @@
 static thread_func start_process NO_RETURN;
 static bool load (const char *cmdline, void (**eip) (void), void **esp);
 
+
 /* Starts a new thread running a user program loaded from
    FILENAME.  The new thread may be scheduled (and may even exit)
    before process_execute() returns.  Returns the new process's
@@ -36,9 +37,9 @@ process_execute (const char *file_name)
 		// exit (-1);
 
   char *fn_copy;
-	/*yangfan*/ 
+	char *fn_copy1;
+	char *program_name;
 	char *saveptr;
-	/*==yangfan*/
 	tid_t tid;
 	
   /* Make a copy of FILE_NAME.
@@ -48,15 +49,18 @@ process_execute (const char *file_name)
     return TID_ERROR;
   strlcpy (fn_copy, file_name, PGSIZE);
 
-	file_name = strtok_r ( (char *)file_name, " ", &saveptr);
+	/* Make a second copy of FILE_NAME for strtok_r(). */
+  fn_copy1 = palloc_get_page (0);
+  if (fn_copy1 == NULL)
+    return TID_ERROR;
+  strlcpy (fn_copy1, file_name, PGSIZE);
 
-	printf ("file_name: %s\n", file_name);
-	printf ("fn_copy: %s\n", fn_copy);
-	printf ("saveptr: %s\n", saveptr);
-	
+	program_name = strtok_r (fn_copy1, " ", &saveptr);
 
   /* Create a new thread to execute FILE_NAME. */
-  tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
+  tid = thread_create (program_name, PRI_DEFAULT, start_process, fn_copy);
+
+	palloc_free_page (fn_copy1);
   if (tid == TID_ERROR) 
     palloc_free_page (fn_copy); 
   return tid;
@@ -67,78 +71,118 @@ process_execute (const char *file_name)
 static void
 start_process (void *file_name_)
 {
-	/*yangfan*/
-	char *saveptr;
-	char *token;
-	char *rest;
-	char **tokens;
-	int args_count;
-	int i;
-	int j;
-	/* == yangfan*/
-
   char *file_name = file_name_;
   struct intr_frame if_;
   bool success;
-  
-  /* Store the pointers to the tokenised arguments in tokens. */
-  tokens = calloc (5, sizeof (char*));
-   
-  if (tokens == NULL) 
+	
+	/* Pointers for strtok_r (). */
+	char *rest;
+	char *saveptr;
+	char *token;
+
+	/* An array of pointer to the char pointers of each token. */
+	char **tokens;
+
+	/* Tokenised file_name. */
+	char *fn_copy;
+
+	int args_count;
+	int i;
+	int numbytes;
+	int totalbytes;
+	void *original_esp;
+	uint8_t word_align;
+
+	fn_copy = palloc_get_page (0);
+  if (fn_copy == NULL)
     thread_exit ();
-  for(i = 0; i < 5; i++) 
-    {
-      tokens[i] = calloc(20, sizeof(char));
-      
-      if (tokens[i] == NULL) 
-        thread_exit ();
-    }
-   
+  strlcpy (fn_copy, file_name, PGSIZE);
+
+	tokens = palloc_get_page (0);
+  if (tokens == NULL)
+    thread_exit ();
+	memset (tokens, '\0', sizeof tokens / sizeof (char *));
+
+	/* Store the pointers to the tokenised arguments in tokens. */
   args_count = 0;
-  
-  for (i = 1, rest = file_name; ; i++, rest = NULL) 
+  for (args_count = 0, rest = fn_copy; ; args_count++, rest = NULL) 
     {
       token = strtok_r (rest, " ", &saveptr);
-      
-      // Break when reach the end of the string. 
+      /* Break when reach the end of the string. */
       if (token == NULL)
-        break;
-      
-      // Find the next argument. 
-      printf("token %d: %s\n", args_count, token);
-      strlcpy(tokens[args_count], token, sizeof (tokens[args_count]));
-      printf ("arg %d: %s\n", args_count, tokens[args_count]);
-      args_count++;
-
-      while (*saveptr == ' ')
-        saveptr++;
+        break;      
+      /* Found the next argument. */
+   		tokens[args_count] = token;
     }
  
-  printf("args_count %d\n", args_count);
-  printf("file_name %s\n", file_name);
-  printf("file_name %p\n", &file_name);
-  //if (args_count > 0)
-  //if (tokens[0] != NULL)
-   // printf ("arg %p\n", 0, &tokens[0]);
-  //for (j = 0; j < args_count; j++)
-    //printf ("arg %d: %s\n", j, tokens[j]);
+  for (i = 0; i < args_count; i++)
+    printf ("arg %d: %s\n", i, tokens[i]);
   
+	printf ("args_count %d\n", args_count);
+
   /* Initialize interrupt frame and load executable. */
   memset (&if_, 0, sizeof if_);
   if_.gs = if_.fs = if_.es = if_.ds = if_.ss = SEL_UDSEG;
   if_.cs = SEL_UCSEG;
   if_.eflags = FLAG_IF | FLAG_MBS;
-  success = load (file_name, &if_.eip, &if_.esp);
+  success = load (tokens[0], &if_.eip, &if_.esp);
   
   /* If load succeeded, set up the stack with the arguments. */
-  
-  
-  for(i = 0; i < 5; i++) 
-      free(tokens[i]);
-  free(tokens);
-  
-  /* If load failed, quit. */
-  palloc_free_page (file_name);
+	if (success)
+		{	
+			original_esp = if_.esp;
+			totalbytes = 0;
+
+			/* Push the arguments onto the stack, one by one, in reverse order. */
+			for (i = args_count - 1; i > 0; i--)
+				{
+					numbytes = (strlen (tokens[i]) + 1) * sizeof (char);
+					if_.esp = if_.esp - numbytes;
+					memcpy (&(* (char *) (if_.esp)), tokens[i], numbytes);
+					totalbytes += numbytes;
+				}
+			
+			/* Round the stack pointer down to a multiple of 4 
+				for best performance. */
+			totalbytes = 4 - totalbytes % 4;
+			word_align = 0;
+			while (totalbytes > 0) 
+				{
+          totalbytes--;
+					if_.esp--;
+					* (uint8_t *)if_.esp = word_align;		
+				}
+			
+			/* Push a null pointer sentinel (0). */
+			if_.esp -= sizeof (char *);
+			* (char *)if_.esp = '\0';
+			
+			/* Push pointers to the arguments (again in reverse). */
+			for (i = args_count - 1; i > 0; i++)
+				{
+					original_esp -= (strlen (tokens[i]) + 1) * sizeof (char);
+					if_.esp -= sizeof (char *);
+					* (void **)if_.esp = original_esp;
+				}
+			
+			/* Push a pointer to the first pointer. */
+			if_.esp -= sizeof (char **);
+			* (char **)if_.esp = if_.esp + sizeof (char **);
+			
+			/* Push the number of arguments. */
+			if_.esp -= sizeof (int);
+			* (int *)if_.esp = args_count - 1;			
+
+			/* Push a fake return address (0). */
+			if_.esp -= 4;
+			* (void **)if_.esp = 0;		
+		}
+
+	palloc_free_page (fn_copy);
+	palloc_free_page (tokens);
+	palloc_free_page (file_name);
+
+  /* If load failed, quit. */  
   if (!success) 
     thread_exit ();
 
